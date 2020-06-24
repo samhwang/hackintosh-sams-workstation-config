1c35e3d2c3fbbe5fc3e91c605254dd6a8a93ceb2
Sam Huynh <samhwang2112.dev@gmail.com>
2020-06-24 16:52:45 +1000

    Update key generating script

--- generate_vault_keys.bash
+++ generate_vault_keys.bash
@@ -1,3 +1,27 @@
 #!/bin/bash
 
+OPENCORE_VERSION=0.5.9
+OPENCORE_EFI_RELEASE=OpenCore-${OPENCORE_VERSION}-RELEASE
+OPENCORE_EFI_FILE=EFI/OC/OpenCore.efi
+OPENCORE_EFI_PATH=../${OPENCORE_EFI_RELEASE}/${OPENCORE_EFI_FILE}
+PROJECT=`pwd`
+
+echo "Removing old keys and vault";
+cd EFI/OC;
+rm -rf Keys OpenCore.efi vault.plist vault.sig;
+cd ../..;
+
+if [ ! -f "$OPENCORE_EFI_PATH" ]; then
+  echo "OpenCore Release file doesn't exist, downloading and extracting.";
+  cd ..
+  curl -LkSso OpenCore-${OPENCORE_VERSION}-RELEASE.zip https://github.com/acidanthera/OpenCorePkg/releases/download/${OPENCORE_VERSION}/${OPENCORE_EFI_RELEASE}.zip;
+  unzip ./${OPENCORE_EFI_RELEASE}.zip -d ${OPENCORE_EFI_RELEASE} ;
+  rm ./${OPENCORE_EFI_RELEASE}.zip
+  cd $PROJECT;
+fi
+
+echo "Copy OpenCore.efi\n";
+cp $OPENCORE_EFI_PATH ./${OPENCORE_EFI_FILE};
+
+echo "Generate keys"
 ./Utilities/CreateVault/sign.command
